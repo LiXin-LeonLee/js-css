bca9ed9ca9a99286eb8a04fa2ba26d4c54246f23
Xin Li <lixin.smart@outlook.com>
2018-03-24 18:53:05 +0800

    Reset my fucking PC and back up my files

---
+++ js/reader/reader.js
@@ -0,0 +1,9 @@
+var tag = document.createElement('div');
+tag.style.cssText = `position:absolute;
+top:50px;
+ right:50px;
+  height:20px;
+  width:20px;
+  background-color: #f7f7f7;`
+
+ducument.body.appendChild(tag);
